0423128ff8ff1cea1d0889e8a035807e72a1051c
Ubuntu <ubuntu@ip-172-31-40-43.us-east-2.compute.internal>
2019-10-19 07:42:33 +0000

    Add a full_title helper

--- spec/controllers/static_pages_controller_spec.rb
+++ spec/controllers/static_pages_controller_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe StaticPagesController, type: :controller do
     it "return http success" do
       get :home
       expect(response).to have_http_status(:success)
-      assert_select "title", "Home | #{base_title}"
+      assert_select "title", "#{base_title}"
     end
   end
 
