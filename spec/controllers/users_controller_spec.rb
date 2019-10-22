8a7d6330a304bd01050ebf092e0cd6f5e8d00232
Ubuntu <ubuntu@ip-172-31-40-43.us-east-2.compute.internal>
2019-10-22 13:18:04 +0000

    Next 6.1.4

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe UsersController, type: :controller do
   describe "GET #new" do
     render_views
-    it "returns http success" do
+    it "new にアクセスできること" do
       get :new
       expect(response).to have_http_status(:success)
     end
